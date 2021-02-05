a67d216e76befb6dea6c6edae8214a7a887c8ae0
prameenadevdas <71130702+prameenadevdas@users.noreply.github.com>
2021-02-05 20:05:08 +0530

    Add files via upload

---
+++ carRacingStage0.5-master/js/Player.js
@@ -0,0 +1,32 @@
+class Player {
+  constructor(){
+    this.index = null
+    this.name = null
+    this.distance = 0
+  }
+
+  getCount(){
+    var playerCountRef = database.ref("playerCount");
+    playerCountRef.on("value",(data) =>{
+      playerCount = data.val();
+      console.log(playerCount)
+    })
+  }
+
+  updateCount(count){
+    database.ref("/").update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,distance:this.distance
+    });
+  }
+  static getPlayerInfo(){
+    var playerRef = database.ref("players")
+    playerRef.on("value",(data)=>{allPlayers = data.val()})
+  }
+}
